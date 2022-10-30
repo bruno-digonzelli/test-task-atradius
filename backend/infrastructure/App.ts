c332e6b26db3627ee358931403ed3157a9d34849
Bruno Digonzelli <bruno.digonzelli@git.com>
2022-10-30 18:23:38 -0300

    7 - Basic request

--- backend/infrastructure/App.ts
+++ backend/infrastructure/App.ts
@@ -6,11 +6,13 @@ import HomepageController from 'application/services/HomepageController';
 import UsersController from 'application/services/UsersController';
 import NotFoundController from 'application/services/NotFoundController';
 import UsersData from './dataSource/UsersData';
+import cors from 'cors';
 
 const app: Application = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
   
 // Home
 app.get('/', (_, res) => HomepageController(METHODS.GET, res));
--- frontend/infrastructure/src/pages/Home.tsx
+++ frontend/infrastructure/src/pages/Home.tsx
@@ -1,11 +1,22 @@
-import { Center, Text } from 'application/UI'
 import React from 'react'
+import { Center, Text } from 'application/UI';
+import useSWR from 'swr'
 
 const Home = () => {
+  // @ts-ignore
+  const fetcher = (...args) => window.fetch(...args).then(res => res.json())
+
+  const { data, error } = useSWR('http://localhost:3000/', fetcher)
+
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
+
+  const { message } = data;
+
   return (
     <Center height={'100vh'}>
         <Text fontSize={'5xl'} color="blue.200" fontWeight={'bold'}>
-          Hello Atradius
+          {message}
         </Text>
     </Center>
   )
